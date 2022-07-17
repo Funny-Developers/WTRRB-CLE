05d6c8848715b4adad701736460c8e174c4fe692
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2022-07-17 23:16:24 +0800

    Start program☆

---
+++ WTRRB-CLE/WTRRB-CLE/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace WTRRB_CLE
+{
+    public class Game
+    {
+        public static string text;
+        public static ConsoleKey key;
+        public static int time;
+
+        public static void TextAppearance(string text)
+        {
+            Console.WriteLine(text);
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else { }
+            }
+        }
+
+        public static void GameStartMenu(string text, ConsoleKey key)
+        {
+            Console.WriteLine(text);
+            while (true)
+            {
+                if (Console.ReadKey().Key == key)
+                {
+                    break;
+                }
+                else { }
+            }
+        }
+
+        public static void AutoText(string text, int time)
+        {
+            Console.WriteLine(text);
+            Thread.Sleep(time);
+        }
+    }
+}
---
+++ WTRRB-CLE/WTRRB-CLE/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace WTRRB_CLE
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------欢迎游玩 在那放射线绽放的地方 ！-------");
+            Game.GameStartMenu("按下回车以开始", ConsoleKey.Enter);
+            Console.WriteLine("--Game Start--");
+            Console.Clear();
+            Game.AutoText("", 3000);
+        }
+    }
+}
